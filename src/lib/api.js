82ef2971b8561851f7a61e082baa8e67f5036246
huynq <qhuy131096@gmail.com>
2019-09-24 17:49:54 +0700

    add location-address

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,13 +1,26 @@
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 const API_KEY = 'd90c45bb95fac0b24519b11a4a9311e2';
-const LOCATION = '10.795017415231317,106.6164487254423'
-const API_URL =`${proxy}https://api.darksky.net/forecast/${API_KEY}/${LOCATION}`;
+const API_URL =`${proxy}https://api.darksky.net/forecast/${API_KEY}/`;
+const GEO_API_URL = `${proxy}https://darksky.net/geo?q=`
+const ADDRESS_API_URL = `${proxy}https://darksky.net/rgeo?hires=1`
 
-function getForeCast() {
-    return fetch(API_URL)
+function getCoordinates(location) {
+    return fetch(`${GEO_API_URL}${location}`)
+            .then(res => res.json())
+}
+
+function getForeCast(lat,lng) {
+    return fetch(`${API_URL}${lat},${lng}`)
+            .then(res => res.json())
+}
+
+function getAddress(lat,lng) {
+    return fetch(`${ADDRESS_API_URL}&lat=${lat}&lon=${lng}`)
             .then(res => res.json())
 }
 
 export default {
-    getForeCast
+    getForeCast,
+    getCoordinates,
+    getAddress
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,74 @@
 <template>
-	<div class="home">
-		<div class="d-flex justify-content-center">
-			<div class="my-4">
-				<div
-					class="card text-white bg-secondary mb-3"
-					style="max-width: 20rem;"
-				>
-					<div class="card-header">Current Weather</div>
-					<div class="card-body">
-						<h4 class="card-title">{{ forecast.currently.summary }}</h4>
-						<p class="card-text temp">
-							{{ forecast.currently.icon }}
-							{{ forecast.currently.temperature }} &#176;F
-						</p>
-						<p class="card-text">
-							{{ forecast.currently.precipProbability }}% change of
-							precipitation
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="home">
+    <div class="d-flex justify-content-center">
+      <div class="my-4">
+        <div class="my-2">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Search" v-model="location"/>
+            <div class="input-group-append">
+              <button class="btn btn-secondary" type="submit" @click="updateLocation">Search</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="card text-white bg-secondary mb-3" style="max-width: 20rem;" v-if="forecast">
+          <div class="card-header">Current Weather in {{ address }}</div>
+          <div class="card-body">
+            <h4 class="card-title">{{ forecast.currently.summary }}</h4>
+            <p class="card-text temp">{{ Math.floor((forecast.currently.temperature - 32)/1.8) }} &#176;C</p>
+            <p class="card-text">
+              {{ forecast.currently.precipProbability }}% change of
+              precipitation
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import API from "@/lib/api";
 export default {
-	name: "home",
+  name: "home",
 
-	data() {
-		return {
-      forecast: {},
+  data() {
+    return {
+      forecast: null,
+      location: localStorage.location || '',
+      address: localStorage.address || '',
+    };
+  },
+  mounted() {
+    this.loadWeather(localStorage.lat || '40.730610',localStorage.lng || '-73.935242');
+  },
+  methods : {
+    loadWeather(lat,lng) {
+      localStorage.lat = lat;
+      localStorage.lng = lng;
+      
+      API.getAddress(lat,lng).then(result => {
+        this.address = [result.name,result.street].join(' ');
+        localStorage.address = this.address;
+      });
+      
+      API.getForeCast(lat,lng).then(result => {
+        this.forecast = result;
+      });
+    },
+
+    updateLocation() {
+      localStorage.location = this.location; 
+      API.getCoordinates(this.location).then(result => 
+        this.loadWeather(result.latitude,result.longitude)
+      );
     }
-	},
-
-	mounted() {
-		API.getForeCast().then(result => {
-			this.forecast = result;
-		});
-	}
+  }
 };
 </script>
 
 <style scoped>
-  .temp {
-    font-size: 2em;
-  }
+.temp {
+  font-size: 2em;
+}
 </style>
